7fe6b2ec0acc2e816e2672426f120985b6c93f3f
QiangZhu <qiangzhu0915@gmail.com>
2018-12-10 18:57:33 -0800

    complete base_options.py

--- options/base_options.py
+++ options/base_options.py
@@ -1,4 +1,8 @@
 import argparse
+import os
+import torch
+from util import util
+
 class BaseOptions():
     def __init__(self):
         self.parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -44,4 +48,41 @@ class BaseOptions():
         if not self.initialized:
             self.initialize()
         opt = self.parser.parse_args()
-        opt.isTrain
+        opt.isTrain = self.isTrain #train or test
+
+        str_ids = opt.gpu_ids.split(',')
+        opt.gpu_ids = []
+        for str_id in str_ids:
+            temp = str_id
+            id = int(str_id)
+            if id >= 0:
+                opt.gpu_ids.append(id)
+
+        #set gpu ids
+        if len(opt.gpu_ids) > 0:
+            torch.cuda.set_device(opt.gpu_ids[0])
+
+        args = vars(opt)
+
+        print('------------ Options -------------')
+        for k, v in sorted(args.items()):
+            print('%s: %s' % (str(k), str(v)))
+        print('-------------- End ----------------')
+
+        if opt.suffix:
+            suffix = ('_' + opt.suffix.format(**vars(opt))) if opt.suffix != '' else ''
+            opt.name = opt.name + suffix
+        # save to the disk
+        expr_dir = os.path.join(opt.checkpoints_dir, opt.name)
+        util.mkdirs(expr_dir)
+        file_name = os.path.join(expr_dir, 'opt.txt')
+        with open(file_name, 'wt') as opt_file:
+            opt_file.write('------------ Options -------------\n')
+            for k, v in sorted(args.items()):
+                opt_file.write('%s: %s\n' % (str(k), str(v)))
+            opt_file.write('-------------- End ----------------\n')
+        self.opt = opt
+        return self.opt
+
+
+
