0806091928596b12601db732383303ac3bceef37
QiangZhu <qiangzhu0915@gmail.com>
2018-12-10 18:56:46 -0800

    update util.py

--- util/util.py
+++ util/util.py
@@ -4,8 +4,8 @@ def mkdirs(paths):
     if isinstance(paths, list) and not isinstance(paths, str):
         for path in paths:
             mkdir(path)
-        else:
-            mkdir(paths)
+    else:
+        mkdir(paths)
 
 def mkdir(path):
     if not os.path.exists(path):
