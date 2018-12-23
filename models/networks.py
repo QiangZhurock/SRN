44411c9b86958807884253a515e1ad159b58cbae
QiangZhu <qiangzhu0915@gmail.com>
2018-12-23 15:29:23 -0800

    add GANLoss in networks.py

--- models/networks.py
+++ models/networks.py
@@ -3,6 +3,17 @@ import torch.nn as nn
 from torch.nn import init
 import functools
 from torch.optim import lr_scheduler
+from torch.autograd import Variable
+
+def get_scheduler(optimizer, opt):
+    if opt.lr_policy == 'lambda':
+        def lambda_rule(epoch):
+            lr_l = 1.0 - max(0, epoch + opt.epoch_count - opt.niter) / float(opt.niter_decay + 1)
+            return lr_l
+        scheduler = lr_scheduler.LambdaLR(optimizer, lr_lambda=lambda_rule)
+    else:
+        raise NotImplementedError('learning rate pplicy [%s] is not implemented', opt.lr_policy)
+    return scheduler
 
 def init_weights(net, init_type='normal', gain=0.02):
     def init_func(m):
@@ -50,6 +61,48 @@ def define_D(input_nc, ndf, which_model_netD, n_layers_D = 3, norm = 'batch',
     else:
         raise NotImplementedError('Discriminator model name [%s] is not recognized' % which_model_netD)
     return init_net(netD, init_type, gpu_ids)
+
+#define the GAN loss which uses either LSGAN.
+class GANLoss(nn.Module):
+    def __init__(self, use_lsgan = True, target_real_label = 1.0, target_fake_label = 0.0,
+                 tensor = torch.FloatTensor):
+        super(GANLoss,self).__init__()
+        #self.register_buffer('real_label', torch.tensor(target_real_label))
+        self.real_label = target_real_label
+        self.fake_label = target_fake_label
+        self.real_label_var = None
+        self.fake_label_var = None
+        self.Tensor = tensor
+        if use_lsgan:
+            self.loss = nn.MSELoss()
+        else:
+            self.loss = nn.BCELoss()
+
+    def get_target_tensor(self, input, target_is_real):
+        target_tensor = None
+        if target_is_real:
+            create_label = ((self.real_label_var is None) or (
+                self.real_label_var.numel() != input.numel()
+            )) #Calculate the number of elements in a PyTorch Tensor by using the PyTorch numel operation.
+            if create_label:
+                real_tensor = self.Tensor(input.size()).fill_(self.real_label)
+                self.real_label_var = Variable(real_tensor, requires_grad = False)
+            target_tensor = self.real_label_var
+        else:
+            create_label = ((self.fake_label_var is None) or (
+                self.fake_label_var.numel() != input.numel()
+            )) #Calculate the number of elements in a PyTorch Tensor by using the PyTorch numel operation.
+            if create_label:
+                fake_tensor = self.Tensor(input.size()).fill_(self.fake_label)
+                self.fake_label_var = Variable(fake_tensor, requires_grad = False)
+            target_tensor = self.fake_label_var
+        return target_tensor
+    def __call__(self, input, target_is_real):
+        target_tensor = self.get_target_tensor(input, target_is_real)
+        return self.loss(input, target_tensor)
+
+
+
 
 #define SRN generator
 class SRNGenerator(nn.Module):
