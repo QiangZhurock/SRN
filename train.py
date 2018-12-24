3d15e5ee54f10fbe6676badfd533339cb2fec683
QiangZhu <qiangzhu0915@gmail.com>
2018-12-23 17:16:24 -0800

    update train.py

---
+++ train.py
@@ -0,0 +1,25 @@
+import time
+from options.train_options import TrainOptions
+from data import CreateDataLoader
+
+if __name__ == '__main__':
+    opt = TrainOptions().parse()
+    data_loader = CreateDataLoader(opt)
+    dataset = data_loader.load_data()
+    dataset_size = len(data_loader)
+
+    total_steps = 0
+
+    for epoch in range(opt.epoch_count, opt.niter + opt.niter_decay + 1):
+        epoch_start_time = time.time()
+        iter_data_time = time.time()
+        epoch_iter = 0
+
+        for i, data in enumerate(dataset):
+            iter_start_time = time.time()
+            if total_steps % opt.print_freq == 0:
+                t_data = iter_start_time - iter_data_time
+            #visualizer.reset()
+            total_steps += opt.batchSize
+            epoch_iter += opt.batchSize
+
